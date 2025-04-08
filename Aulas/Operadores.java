63ccd047e55a5da203522298d2d0845aaeced0f9
Gabs-jg <gabriel19joao@gmail.com>
2025-04-08 16:15:05 -0300

    Operadores - 3

--- Aulas/Operadores.java
+++ Aulas/Operadores.java
@@ -21,5 +21,14 @@ public class Operadores {
 
         concatenacao = "1"+(1+1+1);
         System.out.println(concatenacao);
+
+        int numero = 5;
+        System.out.println(- numero);
+
+        numero = - numero;
+        System.out.println(numero);
+
+        numero = numero *-1;
+        System.out.println(numero);
     }
 }
