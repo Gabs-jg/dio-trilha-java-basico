218c55b9d2750f4dc7061c3556c46c8f2b2844d8
Gabs-jg <gabriel19joao@gmail.com>
2025-04-08 16:11:59 -0300

    Operadores - 2

---
+++ Aulas/Operadores.java
@@ -0,0 +1,25 @@
+package Aulas;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+    }
+}
